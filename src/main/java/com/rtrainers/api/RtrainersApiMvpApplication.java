8e4f29f89e127ccceffc388b3fb522248682c641
Giulivan Sousa <giulivan_cs@hotmail.com>
2026-06-25 13:32:48 -0300

    chore: inicialização projeto Spring Boot com Spring Web e Spring Security

---
+++ src/main/java/com/rtrainers/api/RtrainersApiMvpApplication.java
@@ -0,0 +1,13 @@
+package com.rtrainers.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtrainersApiMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtrainersApiMvpApplication.class, args);
+	}
+
+}
